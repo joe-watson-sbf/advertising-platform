845c21ca3903a949559d0d011023b37a868d328d
Joe Watson SBF <joewatson40@hotmail.com>
2023-03-18 01:14:08 -0500

    new components added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import '../styles/main.scss'
 
 export const metadata = {
@@ -12,6 +13,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+      </Head>
       <body>{children}</body>
     </html>
   )
